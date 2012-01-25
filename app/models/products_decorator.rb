5ae214952cc02b23829b13363fc099ffc5b0eb85
Tim Neems <tneems@gmail.com>
2012-01-25 11:35:56 -0500

    Add validations

---
+++ app/models/products_decorator.rb
@@ -0,0 +1,6 @@
+Product.class_eval do
+  validates :google_merchant_gtin, :length => { :maximum => 255 }
+  validates :google_merchant_brand, :length => { :maximum => 255 }
+  validates :google_merchant_product_category, :length => { :maximum => 255 }
+  validates :google_merchant_product_type, :length => { :maximum => 255 }
+end
