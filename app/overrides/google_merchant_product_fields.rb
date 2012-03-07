cb6adcfb304c60b953d50441018adec0b9b152f9
Tim Neems <tneems@gmail.com>
2012-03-07 08:57:39 -0500

    Fix product edit form

--- app/overrides/google_merchant_product_fields.rb
+++ app/overrides/google_merchant_product_fields.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(:virtual_path => "spree/admin/products/_form",
                      :name => "google_merchant_product_fields",
                      :insert_bottom => "[data-hook='admin_product_form_additional_fields']",
-                     :partial => "admin/products/google_merchant_fields")
+                     :partial => "spree/admin/products/google_merchant_fields")
