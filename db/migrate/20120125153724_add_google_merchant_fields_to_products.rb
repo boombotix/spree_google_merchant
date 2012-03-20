d4231a600a6a0384db65bc489182390e0629f2e0
Tim Neems <tneems@gmail.com>
2012-03-20 08:17:55 -0400

    Move some fields from variant to product

--- db/migrate/20120125153724_add_google_merchant_fields_to_products.rb
+++ db/migrate/20120125153724_add_google_merchant_fields_to_products.rb
@@ -1,8 +1,5 @@
 class AddGoogleMerchantFieldsToProducts < ActiveRecord::Migration
   def change
     add_column :spree_variants, :google_merchant_gtin, :string
-    add_column :spree_variants, :google_merchant_brand, :string
-    add_column :spree_variants, :google_merchant_product_category, :string
-    add_column :spree_variants, :google_merchant_product_type, :string
   end
 end
