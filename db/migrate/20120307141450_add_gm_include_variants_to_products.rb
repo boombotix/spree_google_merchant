5d5106d290c5ea192484681e077db03084177f8b
tylerhoran <tylerhoran@gmail.com>
2013-01-27 12:37:33 -0500

    change var

--- db/migrate/20120307141450_add_gm_include_variants_to_products.rb
+++
@@ -1,8 +0,0 @@
-class AddGmIncludeVariantsToProducts < ActiveRecord::Migration
-  def change
-    add_column :spree_products, :google_merchant_include_variants, :boolean, :default => false
-    add_column :spree_products, :google_merchant_brand, :string
-    add_column :spree_products, :google_merchant_product_category, :string
-    add_column :spree_products, :google_merchant_product_type, :string
-  end
-end
