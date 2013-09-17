581d83db12a4c16c2bd63a25dcbce0f66d59810d
Ben Radler <ben@benradler.com>
2013-09-16 23:49:25 -0700

    rails 4 route compatibility

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Spree::Core::Engine.routes.prepend do
-  match "google_merchant", :to => 'products#google_merchant'
+  match "google_merchant", :to => 'products#google_merchant', :via => [:get, :post]
 
   namespace :admin do
     resource :google_merchants
