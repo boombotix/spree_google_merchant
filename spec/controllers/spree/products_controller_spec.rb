6bf59ed2fd6b12641f0040e30f337dc9347ec394
Ben Radler <ben@benradler.com>
2014-03-02 12:11:04 -0800

    specs for gem
    Conflicts:
    	spec/controllers/spree/products_controller_spec.rb

--- spec/controllers/spree/products_controller_spec.rb
+++ spec/controllers/spree/products_controller_spec.rb
@@ -2,9 +2,12 @@ require 'spec_helper'
 
 describe Spree::ProductsController do
   describe :google_merchant do
+    let!(:product) { create(:product, available_on: 1.year.ago) }
+
     it 'sets @products instance variable' do
-      get :google_merchant, format: :rss
+      spree_get :google_merchant, format: :rss
       assigns(:products).should_not be_nil
+      assigns(:products).first.should eql(product)
     end
   end
 end
