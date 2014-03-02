76b3adb56678ac280c54f3b309ec9df2e4d768d8
Ben Radler <ben@benradler.com>
2014-03-02 00:04:13 -0800

    bring back our .rss route

--- app/controllers/spree/products_controller_decorator.rb
+++ app/controllers/spree/products_controller_decorator.rb
@@ -2,6 +2,8 @@ module Spree
   ProductsController.class_eval do
     def google_merchant
       @products = Product.active
+      # respond_to :rss
+      # respond_with @products
     end
   end
 end
