bd916eeea0cd26ec18469410d008547d90b8e465
Ben Radler <ben@benradler.com>
2014-03-02 15:32:06 -0800

    Fix tpyo

--- app/overrides/google_merchant_admin_configurations_menu.rb
+++ app/overrides/google_merchant_admin_configurations_menu.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
   virtual_path:  "spree/admin/shared/_configuration_menu",
-  name:          "google_merchange_admin_configurations_menu",
+  name:          "google_merchant_admin_configurations_menu",
   insert_bottom: "[data-hook='admin_configurations_sidebar_menu'], #admin_configurations_sidebar_menu[data-hook]",
   text:          "<li<%== ' class=\"active\"' if controller.controller_name == 'theme_settings' %>><%= link_to \"Google Merchant\", admin_google_merchants_path %></li>",
   disabled:      false
