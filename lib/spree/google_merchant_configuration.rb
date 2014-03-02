05863cc519721b33933ca96231cca408b56f6518
Ben Radler <ben@benradler.com>
2014-03-02 00:00:41 -0800

    code cleanup

--- lib/spree/google_merchant_configuration.rb
+++ lib/spree/google_merchant_configuration.rb
@@ -1,5 +1,5 @@
 class Spree::GoogleMerchantConfiguration < Spree::Preferences::Configuration
-  preference :google_merchant_title, :string, :default => 'My Spree Shop'
-  preference :google_merchant_description, :string, :default => 'List of our products'
-  preference :production_domain, :string, :default => 'http://www.myspreeshop.com/'
+  preference :google_merchant_title,       :string, default: 'My Spree Store'
+  preference :google_merchant_description, :string, default: 'RSS feed of our products'
+  preference :production_domain,           :string, default: 'http://spreecommerce.com/'
 end
