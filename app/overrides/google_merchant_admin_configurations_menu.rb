5e92c490bc774a979c01b72f9678fbeec8b38291
Evan Pavlica <evan@xicreative.net>
2015-04-30 18:24:26 -0700

    Update views & translations for Spree 3

--- app/overrides/google_merchant_admin_configurations_menu.rb
+++ app/overrides/google_merchant_admin_configurations_menu.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(
-  virtual_path:  "spree/admin/shared/_configuration_menu",
+  virtual_path:  "spree/admin/shared/sub_menu/_configuration",
   name:          "google_merchant_admin_configurations_menu",
   insert_bottom: "[data-hook='admin_configurations_sidebar_menu'], #admin_configurations_sidebar_menu[data-hook]",
   text:          "<li<%== ' class=\"active\"' if controller.controller_name == 'theme_settings' %>><%= link_to \"Google Merchant\", admin_google_merchants_path %></li>",
